bf4dcbe0fe8c12183be2a23d10602e250e979c20
Sam Atkinson <sammodeste@yahoo.com>
2020-07-10 20:06:04 +0100

    Used querySelector for btn_reset

--- js/app.js
+++ js/app.js
@@ -1,3 +1,3 @@
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
-const btn_rest = document.getElementsByClassName('btn_reset');
+const btn_reset = document.querySelector('.btn_reset');
