5c637c1e79656c4ab97fac14947b704dd056d2c5
Sam Atkinson <sammodeste@yahoo.com>
2020-09-28 17:37:51 +0100

    Added Comments of functions to create

--- js/app.js
+++ js/app.js
@@ -2,3 +2,13 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const btn_reset = document.querySelector('.btn_reset');
 let missed = 0;
+
+//1.Create an array of pharses
+
+//2. List of Functions to create
+ //2A. Return a random pharse from the pharse array named: const getRandomPhraseArray
+ //2B. Add the letters to of a string to the display named: const addPhraseToDisplay
+ //2C. Check if a letter is in the phrase named: const checkletter
+ //2D. Check if the game has been won or lost
+//2E. Attach an event listner to the START BUTTON which is startButton.addEventListener('click', () => {}); 
+//2F. Listen for the onscreen keyboard to be clicked qwerty.addEventListener('click', () => {});
