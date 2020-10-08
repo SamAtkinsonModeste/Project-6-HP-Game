eeb2167e6932d0a516d1e3e6c05179683f1c2e1c
Sam Atkinson <sammodeste@yahoo.com>
2020-10-08 21:46:32 +0100

    my rubbish js now which will be final version

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 const qwerty = document.getElementById('qwerty');
+const banner = document.querySelector('.header');
 const phrase = document.getElementById('phrase');
-const btn_reset = document.querySelector('.btn_reset');
+const ul     = document.querySelector('ul');
+const startBtn = document.querySelector('a');
 let missed = 0;
 
+ 
 //1.Create an array of pharses
 const phrases = [
     
@@ -38,52 +41,109 @@ const phrases = [
 ];
 
 
+const pets = [
+  {
+    hint: "dog",
+    phares: "Michael",
+  },
+
+  {
+    hint: "cat",
+    phares: "Sue",
+  },
+
+  {
+    hint: "horse",
+    pharse: "Richard",
+  },
+
+];
+
 //2. List of Functions to create
  //2A. Return a random pharse from the pharse array named: const getRandomPhraseArray
   const getRandomPhraseArray = (arr) => { 
-    //The square brackets around the Math methods 
+    
+    //The square brackets around the Math methods get the index of the array item
     let randomArrItem = arr[Math.floor(Math.random() * arr.length)];
     return randomArrItem;
   };
 
-  // console.log(getRandomPhraseArray(phrases));
+  // console.log(getRandomPhraseArray(Object.values(pets)));
 
-//2B. Add the letters to of a string to the display named: const addPhraseToDisplay
-const addPhraseToDisplay = (letters) => {
-  letters = getRandomPhraseArray(phrases);
-   let hintsLetters = Object.values(letters);
-   
-   //  return hintsLetters; 
-   for (let i = 0; i< hintsLetters.length; i++) {
-     //This gets my hint text
-        let hintText = hintsLetters[0];
-        let hintQuestion = document.createElement('h2');
-        hintQuestion.textContent = hintText;
-        console.log(hintQuestion);
-      //This gets the pharse letters
-        let pharseWords = hintsLetters[1];
-        console.log(pharseWords);
-        for (let j =0; j< pharseWords.length; j++) {
-             const letter = pharseWords[j];
-             const spanLetter = document.createElement('span');
-             spanLetter.textContent = letter;
-             spanLetter.className = "letter";
-             if (spanLetter.textContent === '') {
-              spanLetter.className ="space";
-            }
-             console.log(spanLetter);
 
-        }
+  //2B. Add the letters to of a string to the display named: const addPhraseToDisplay
+  const addPhraseToDisplay = (arr) => {
+    
+      const li = document.createElement('li');
+      //create elements to nest in li
+      function createElement( nameElement, property, value ) {
+        const element = document.createElement(nameElement);
+        element[property] = value;
+        return element;
+    }
 
-        
-        
-   }
-   
-   
-   return letters;
-};
+    
 
-console.log(addPhraseToDisplay());
+       for (let i = 0; i <arr.length; i++) {
+             let hintPharse = arr[i];
+             
+            banner.textContent = hintPharse.hint;
+          
+            li.textContent = hintPharse.pharse;
+            
+            
+                        
+          //     let secondPropertyVals =  Object.values(arr[i])
+          // for (let j= 0; j< secondPropertyVals.length; j++) {
+                   
+          //         let liItem = secondPropertyVals[1][j];
+          //         li.textContent = liItem;
+          //      }
+              
+
+       }
+    
+      const spanBox = document.createElement('span');
+      spanBox.className = "box";
+      const spanFront = document.createElement('span');
+      spanFront.className = "front";
+  // spanFront.textContent = "F";
+     const spanLetter = document.createElement('span');
+      spanLetter.className = "letter";
+  // spanLetter.textContent = "L";
+     spanBox.appendChild(spanFront);
+     spanBox.appendChild(spanLetter);
+     li.appendChild(spanBox);
+      ul.appendChild(li);
+
+      return ul;
+     
+  }
+
+
+ 
+console.log( addPhraseToDisplay(pets));
+ 
+
+
+
+
+console.log(startBtn);
+
+//Start Game Button Listener
+startBtn.addEventListener('click', (evt) => {
+  
+  const overlay = document.getElementById('overlay');
+    
+   overlay.style.display = "none";
+ 
+
+});
+
+
+
+
+
  //2C. Check if a letter is in the phrase named: const checkletter
  //2D. Check if the game has been won or lost
 //2E. Attach an event listner to the START BUTTON which is startButton.addEventListener('click', () => {}); 
